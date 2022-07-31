7f88e864c63761c35beb9574f6394a029ff04c9c
homosapiens <86591199+GANZAZz@users.noreply.github.com>
2022-07-31 13:58:07 +0800

    ЗАКИНУЛ кривой скрипт передвижения (позже перепишу ибо работает некоректно)

--- Assets/scripts/playerControllerKeyboard.cs
+++ Assets/scripts/playerControllerKeyboard.cs
@@ -2,5 +2,34 @@ using UnityEngine;
 
 public class playerControllerKeyboard : MonoBehaviour
 {
+    public playerMovement playerMovement;
 
+    private void Start()
+    {
+        playerMovement = playerMovement == null ? GetComponent<playerMovement>() : playerMovement;
+        if (playerMovement == null)
+        {
+            Debug.LogError("Player not set to conroller");
+        }
+    }
+
+    private void Update()
+    {
+        if (playerMovement != null)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                playerMovement.MoveRight();
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                playerMovement.MoveLeft();
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                playerMovement.Jump();
+            }
+
+        }
+    }
 }
--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -4,12 +4,89 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private float speed = 3.5f;
+    [Header("Speed")]
+    public float runSpeed = 3.5f;
+    public float JumpForce = 5f;
+
+
     private Rigidbody2D _Rigidbody;
+    private MoveState _moveState = MoveState.Idle;
+    private DirectionState _directionState = DirectionState.Right;
+    private Transform _transform;
+    private float _walkTime = 0, _walkCooldown = 0.2f;
+
+    public void MoveRight()
+    {
+        if (_moveState != MoveState.Jump)
+        {
+            _moveState = MoveState.Walk;
+            if (_directionState == DirectionState.Left)
+            {
+                _transform.localScale = new Vector3(-_transform.localScale.x, _transform.lossyScale.y, _transform.localScale.y);
+                _directionState = DirectionState.Right;
+            }
+            _walkTime = _walkCooldown;
+        }
+    }
+
+    public void MoveLeft()
+    {
+        if (_moveState != MoveState.Jump)
+        {
+            _moveState = MoveState.Walk;
+            if (_directionState == DirectionState.Right)
+            {
+                _transform.localScale = new Vector3(-_transform.localScale.x, _transform.localScale.y, _transform.localScale.z);
+                _directionState = DirectionState.Left;
+            }
+            _walkTime = _walkCooldown;
+        }
+    }
+
+    public void Jump()
+    {
+        if (_moveState != MoveState.Jump)
+        {
+            _Rigidbody.velocity = (Vector3.up * JumpForce * Time.deltaTime);
+            _moveState = MoveState.Jump;
+        }
+    }
+
+    private void Idle()
+    {
+        _moveState = MoveState.Idle;
+    }
 
     private void Start()
     {
+        _transform = GetComponent<Transform>();
         _Rigidbody = GetComponent<Rigidbody2D>();
+        _directionState = transform.localScale.x > 0 ? DirectionState.Right : DirectionState.Left;
     }
+
+    private void Update()
+    {
+        if (_moveState == MoveState.Jump)
+        {
+            if(_Rigidbody.velocity == Vector2.zero)
+            {
+                Idle();
+            }
+        }
+    }
+
+    enum DirectionState
+    {
+        Right,
+        Left
+    }
+
+    enum MoveState
+    {
+        Idle,
+        Walk,
+        Jump
+    }
+
 
 }
